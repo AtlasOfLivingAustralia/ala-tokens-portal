ff41c482bbac40d25813bf3033cbd674970d04e3
Jack Brinkman <jack.brinkman@csiro.au>
2022-07-08 16:10:56 +1000

    First working implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider, Global } from '@mantine/core';
 
 import {
   getRedirectResult,
@@ -12,9 +13,44 @@ function App(): React.ReactElement {
   getRedirectResult();
 
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <MantineProvider
+      theme={{
+        colorScheme: 'dark',
+        fontFamily: 'Roboto, sans-serif',
+        headings: {
+          fontFamily: 'Lato, sans-serif',
+        },
+        colors: {
+          rust: [
+            '#000000',
+            '#000000',
+            '#FDEBE7',
+            '#FAC7BC',
+            '#F7A392',
+            '#F47F67',
+            '#F15B3C',
+            '#EE3711',
+            '#BE2C0E',
+            '#8F210A',
+          ],
+        },
+      }}
+      withGlobalStyles
+    >
+      <Global
+        styles={(theme) => ({
+          body: {
+            backgroundColor:
+              theme.colorScheme === 'dark'
+                ? theme.colors.dark[7]
+                : theme.colors.gray[0],
+          },
+        })}
+      />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </MantineProvider>
   );
 }
 
--- src/routes/Auth/index.tsx
+++ src/routes/Auth/index.tsx
@@ -1,48 +1,96 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
+import {
+  Card,
+  Container,
+  Center,
+  Title,
+  Image,
+  Text,
+  Box,
+  Button,
+} from '@mantine/core';
 
 import {
   getClient,
   getToken,
   signInWithRedirect,
   signOut,
+  OIDCAuthResult,
   // eslint-disable-next-line import/no-relative-packages
 } from '../../../../ala-web-auth/dist';
 
 function Auth(): ReactElement {
-  const token = getToken();
+  const [token, setToken] = useState<OIDCAuthResult | null>(getToken());
 
-  const login = (): void => {
-    const client = getClient(
-      'oidc-test-client-id',
-      ['openid', 'email'],
-      'test'
-    );
-    signInWithRedirect(client, 'http://localhost:3000');
+  const onClick = (): void => {
+    if (!token) {
+      const client = getClient(
+        'oidc-test-client-id',
+        ['openid', 'email', 'profile', 'users:read'],
+        'test'
+      );
+      signInWithRedirect(client, 'http://localhost:3000');
+    } else {
+      signOut();
+      setToken(null);
+    }
   };
 
+  console.log(token);
+
   // If we're waiting for an auth state update
-  return token ? (
-    <div>
-      <div>Auth (Logged In)</div>
-      <div>Token: {JSON.stringify(token)}</div>
-      <button
-        onClick={() => {
-          signOut();
-          window.location.reload();
-        }}
-        type="button"
-      >
-        Logout
-      </button>
-    </div>
-  ) : (
-    <div>
-      <div>Auth (Logged Out)</div>
-      <button onClick={login} type="button">
-        Login
-      </button>
-    </div>
+  return (
+    <Container fluid style={{ display: 'flex' }}>
+      <Center>
+        <Card shadow="lg" radius="xl" style={{ minWidth: 350, minHeight: 300 }}>
+          <Box px={16} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Card.Section
+              style={{ display: 'flex', justifyContent: 'center' }}
+              py={12}
+            >
+              <Image
+                src="https://www.ala.org.au/app/uploads/2020/06/ALA_Logo_Mark-only.png"
+                width={75}
+                height={75}
+              />
+            </Card.Section>
+            <Title align="center" order={3} mt={6}>
+              ALA SPA Auth
+            </Title>
+            <Text align="center" color="dimmed" mt={12}>
+              {token ? 'You are authenticated' : 'You are not authenticated'}
+            </Text>
+            <Button color="rust" mt={32} onClick={onClick}>
+              {token ? 'Sign Out' : 'Sign In'}
+            </Button>
+          </Box>
+        </Card>
+      </Center>
+    </Container>
   );
+
+  //   return token ? (
+  //     <div>
+  //       <div>Auth (Logged In)</div>
+  //       <div>Token: {JSON.stringify(token)}</div>
+  //       <button
+  //         onClick={() => {
+  //           signOut();
+  //           window.location.reload();
+  //         }}
+  //         type="button"
+  //       >
+  //         Logout
+  //       </button>
+  //     </div>
+  //   ) : (
+  //     <div>
+  //       <div>Auth (Logged Out)</div>
+  //       <button onClick={login} type="button">
+  //         Login
+  //       </button>
+  //     </div>
+  //   );
 }
 
 export default Auth;
