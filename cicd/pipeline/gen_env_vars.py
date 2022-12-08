9b986816c52d341f3364dec5d9544b053a9fb001
Joe Lipson <joe.lipson@csiro.au>
2022-12-08 15:24:00 +1100

    cicd additions

---
+++ cicd/pipeline/gen_env_vars.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+###
+# Genarate the environment vars file for the current environment.
+# Usage: gen_env_vars.py --env [development|testing|staging|production] --conf [/path/to/conf.ini]
+
+import argparse
+import configparser
+import os
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--env", "-e", help="The environment we are running in", required=True)
+parser.add_argument("--conf", "-c", help="Path to the config file. Default: config.ini", required=False, default="config.ini")
+
+args = parser.parse_args()
+
+# read the config file
+config = configparser.ConfigParser(defaults={ 'CLEAN_BRANCH' : os.environ['CLEAN_BRANCH'] }, interpolation=configparser.ExtendedInterpolation())
+config.read(args.conf)
+
+# get values for the relevant environment
+env_config = config[args.env]
+
+# print key/vals
+for key in env_config:
+  print(f"{key.upper()}={env_config[key]}")
+
